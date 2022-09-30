23c0fbd13a19026427864a176201601c7df948b4
EWASJON <ewasjon@ericsson.com>
2022-09-30 08:53:58 +0200

    fix: added missing docker file
    Using 'build-with-gcc4.8.sh' failed because the base docker image
    which includes gcc 4.8 was not include in the project and thus not
    getting compiled.

--- build-with-gcc4.8.sh
+++ build-with-gcc4.8.sh
@@ -7,4 +7,5 @@ cp -r src build/docker/.
 cp -r libs build/docker/.
 cp CMakeLists.txt build/docker/.
 
+sudo docker build . -f docker/Dockerfile.gcc-4.8 -t build:gcc-4.8
 sudo docker build ./build -f docker/Dockerfile.build.gcc-4.8
