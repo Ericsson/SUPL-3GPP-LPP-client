720951f3431c39097e2f4e5745b7a4390bc23f3d
EWASJON <ewasjon@ericsson.com>
2025-04-14 09:24:31 +0200

    rel: 4.0.12

--- dependency/time/include/time/timestamp.hpp
+++ dependency/time/include/time/timestamp.hpp
@@ -99,7 +99,7 @@ struct TimePoint {
 class LeapSeconds {
 public:
     NODISCARD static int64_t count() {
-#define LAST_UPDATE_CHECKED 0x04000B
+#define LAST_UPDATE_CHECKED 0x04000C
 #ifndef CLIENT_VERSION_INT
 #error "Please define CLIENT_VERSION_INT"
 #endif
