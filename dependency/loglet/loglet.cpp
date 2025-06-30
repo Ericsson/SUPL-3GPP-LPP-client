ae61442ce701bd83253dde1088c9a9eb0b0ba8de
Axel Sundelin <axel.sundelin@ericsson.com>
2025-06-30 10:47:46 +0200

    Make foreground color reset color instead of no op

--- dependency/loglet/loglet.cpp
+++ dependency/loglet/loglet.cpp
@@ -16,7 +16,7 @@
 #define COLOR_MAGENTA "\033[35m"
 #define COLOR_CYAN "\033[36m"
 #define COLOR_WHITE "\033[37m"
-#define COLOR_FOREGROUND ""
+#define COLOR_FOREGROUND "\033[0m"
 #define COLOR_BOLD "\033[1m"
 #define COLOR_UNDERLINE "\033[4m"
 #define COLOR_GREEN "\033[32m"
