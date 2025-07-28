f3c8a042b39156a61257009b4dbbe69efe4a294b
Axel Sundelin <axel.sundelin@ericsson.com>
2025-07-28 16:04:46 +0200

    clean up RTCM1042

--- dependency/format/rtcm/include/format/rtcm/1042.hpp
+++ dependency/format/rtcm/include/format/rtcm/1042.hpp
@@ -28,38 +28,8 @@ public:
 
     NODISCARD static std::unique_ptr<Message> parse(std::vector<uint8_t> data);
 
-// private:
+private:
     EXPLICIT Rtcm1042Message(std::vector<uint8_t> data) NOEXCEPT;
-    DF009  prn;
-    DF076  week;
-    DF077  SV_ACCURACY;
-    DF078  code_on_l2;
-    DF079  idot;
-    DF071  iode;
-    DF081  t_oc;
-    DF082  a_f2;
-    DF083  a_f1;
-    DF084  a_f0;
-    DF085  iodc;
-    DF086  C_rs;
-    DF087  dn;
-    DF088  M_0;
-    DF089  C_uc;
-    DF090  e;
-    DF091  C_us;
-    DF092  sqrt_A;
-    DF093  t_oe;
-    DF094  C_ic;
-    DF095  OMEGA_0;
-    DF096  C_is;
-    DF097  i_0;
-    DF098  C_rc;
-    DF099  omega;
-    DF100  OMEGADOT;
-    DF101  t_GD;
-    DF102  SV_HEALTH;
-    DF103  L2_P_data_flag;
-    DF137  fit;
 };
 
 }  // namespace rtcm
--- dependency/format/rtcm/messages/1042.cpp
+++ dependency/format/rtcm/messages/1042.cpp
@@ -15,8 +15,6 @@ Rtcm1042Message::Rtcm1042Message(std::vector<uint8_t> mData) NOEXCEPT
     : Message{mData} {}
 
 void Rtcm1042Message::print() const NOEXCEPT {
-    printf("[%4d]\n",           static_cast<int>(mType));
-    printf("        prn: %d\n", static_cast<int>(prn));
 }
 
 std::unique_ptr<Message> Rtcm1042Message::clone() const NOEXCEPT {
@@ -44,36 +42,6 @@ std::unique_ptr<Message> Rtcm1042Message::parse(std::vector<uint8_t> mData) {
         ERRORF("bits: %s", bits.to_string().c_str());
         return std::make_unique<ErrorMessage>();
     }
-    getdatafield(bits,i,  m->prn           );
-    getdatafield(bits,i,  m->week          );
-    getdatafield(bits,i,  m->SV_ACCURACY   );
-    getdatafield(bits,i,  m->code_on_l2    );
-    getdatafield(bits,i,  m->idot          );
-    getdatafield(bits,i,  m->iode          );
-    getdatafield(bits,i,  m->t_oc          );
-    getdatafield(bits,i,  m->a_f2          );
-    getdatafield(bits,i,  m->a_f1          );
-    getdatafield(bits,i,  m->a_f0          );
-    getdatafield(bits,i,  m->iodc          );
-    getdatafield(bits,i,  m->C_rs          );
-    getdatafield(bits,i,  m->dn            );
-    getdatafield(bits,i,  m->M_0           );
-    getdatafield(bits,i,  m->C_uc          );
-    getdatafield(bits,i,  m->e             );
-    getdatafield(bits,i,  m->C_us          );
-    getdatafield(bits,i,  m->sqrt_A        );
-    getdatafield(bits,i,  m->t_oe          );
-    getdatafield(bits,i,  m->C_ic          );
-    getdatafield(bits,i,  m->OMEGA_0       );
-    getdatafield(bits,i,  m->C_is          );
-    getdatafield(bits,i,  m->i_0           );
-    getdatafield(bits,i,  m->C_rc          );
-    getdatafield(bits,i,  m->omega         );
-    getdatafield(bits,i,  m->OMEGADOT      );
-    getdatafield(bits,i,  m->t_GD          );
-    getdatafield(bits,i,  m->SV_HEALTH     );
-    getdatafield(bits,i,  m->L2_P_data_flag);
-    getdatafield(bits,i,  m->fit           );
 
     return std::unique_ptr<Rtcm1042Message>(m);
 }
