064af31b2983e66bf058cba95ca30c6d6473df48
Pontus Hedman <pontus.hedman@ericsson.com>
2025-07-28 14:20:11 +0200

    New files for beidou and galileo

---
+++ dependency/format/rtcm/include/format/rtcm/1042.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <format/rtcm/message.hpp>
+
+#include <cmath>
+#include <memory>
+
+#include <time/tai.hpp>
+#include "datafields.hpp"
+
+// TODO: Put headers for messages in /messages folder
+// Also maybe remove Message in Rtcm1042Message to be more uniform with other formats
+
+namespace format {
+namespace rtcm{
+
+class Rtcm1042Message final : public Message {
+public:
+    ~Rtcm1042Message() override = default;
+
+    Rtcm1042Message(Rtcm1042Message const& other)
+        : Message(other) {}
+    Rtcm1042Message(Rtcm1042Message&&)                 = delete;
+    Rtcm1042Message& operator=(Rtcm1042Message const&) = delete;
+    Rtcm1042Message& operator=(Rtcm1042Message&&)      = delete;
+
+    void                     print() const NOEXCEPT override;
+    std::unique_ptr<Message> clone() const NOEXCEPT override;
+
+    NODISCARD static std::unique_ptr<Message> parse(std::vector<uint8_t> data);
+
+// private:
+    EXPLICIT Rtcm1042Message(std::vector<uint8_t> data) NOEXCEPT;
+    DF009  prn;
+    DF076  week;
+    DF077  SV_ACCURACY;
+    DF078  code_on_l2;
+    DF079  idot;
+    DF071  iode;
+    DF081  t_oc;
+    DF082  a_f2;
+    DF083  a_f1;
+    DF084  a_f0;
+    DF085  iodc;
+    DF086  C_rs;
+    DF087  dn;
+    DF088  M_0;
+    DF089  C_uc;
+    DF090  e;
+    DF091  C_us;
+    DF092  sqrt_A;
+    DF093  t_oe;
+    DF094  C_ic;
+    DF095  OMEGA_0;
+    DF096  C_is;
+    DF097  i_0;
+    DF098  C_rc;
+    DF099  omega;
+    DF100  OMEGADOT;
+    DF101  t_GD;
+    DF102  SV_HEALTH;
+    DF103  L2_P_data_flag;
+    DF137  fit;
+};
+
+}  // namespace rtcm
+}  // namespace format
---
+++ dependency/format/rtcm/include/format/rtcm/1046.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <format/rtcm/message.hpp>
+
+#include <cmath>
+#include <memory>
+
+#include <time/tai.hpp>
+#include "datafields.hpp"
+
+// TODO: Put headers for messages in /messages folder
+// Also maybe remove Message in Rtcm1046Message to be more uniform with other formats
+
+namespace format {
+namespace rtcm{
+
+class Rtcm1046Message final : public Message {
+public:
+    ~Rtcm1046Message() override = default;
+
+    Rtcm1046Message(Rtcm1046Message const& other)
+        : Message(other) {}
+    Rtcm1046Message(Rtcm1046Message&&)                 = delete;
+    Rtcm1046Message& operator=(Rtcm1046Message const&) = delete;
+    Rtcm1046Message& operator=(Rtcm1046Message&&)      = delete;
+
+    void                     print() const NOEXCEPT override;
+    std::unique_ptr<Message> clone() const NOEXCEPT override;
+
+    NODISCARD static std::unique_ptr<Message> parse(std::vector<uint8_t> data);
+
+// private:
+    EXPLICIT Rtcm1046Message(std::vector<uint8_t> data) NOEXCEPT;
+    DF009  prn;
+    DF076  week;
+    DF077  SV_ACCURACY;
+    DF078  code_on_l2;
+    DF079  idot;
+    DF071  iode;
+    DF081  t_oc;
+    DF082  a_f2;
+    DF083  a_f1;
+    DF084  a_f0;
+    DF085  iodc;
+    DF086  C_rs;
+    DF087  dn;
+    DF088  M_0;
+    DF089  C_uc;
+    DF090  e;
+    DF091  C_us;
+    DF092  sqrt_A;
+    DF093  t_oe;
+    DF094  C_ic;
+    DF095  OMEGA_0;
+    DF096  C_is;
+    DF097  i_0;
+    DF098  C_rc;
+    DF099  omega;
+    DF100  OMEGADOT;
+    DF101  t_GD;
+    DF102  SV_HEALTH;
+    DF103  L2_P_data_flag;
+    DF137  fit;
+};
+
+}  // namespace rtcm
+}  // namespace format
