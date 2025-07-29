af21ef521fa19a4e167b4916e829060e1fab6842
Pontus Hedman <pontus.hedman@ericsson.com>
2025-07-29 09:51:39 +0200

    Started on beidou support

--- dependency/format/rtcm/include/format/rtcm/1042.hpp
+++ dependency/format/rtcm/include/format/rtcm/1042.hpp
@@ -27,6 +27,35 @@ public:
 
     NODISCARD static std::unique_ptr<Message> parse(std::vector<uint8_t> data);
 
+    DF488 prn;
+    DF489 week_number;
+    DF490 ura_index;
+    DF491 idot;
+    DF492 aode;
+    DF493 toc;
+    DF494 af2;
+    DF495 af1;
+    DF496 af0;
+    DF497 aodc;
+    DF498 crs;
+    DF499 delta_n;
+    DF500 m0;
+    DF501 cuc;
+    DF502 e;
+    DF503 cus;
+    DF504 sqrt_a;
+    DF505 toe;
+    DF506 cic;
+    DF507 omega0;
+    DF508 cis;
+    DF509 i0;
+    DF510 crc;
+    DF511 omega;
+    DF512 omega_dot;
+    DF513 tgd1;
+    DF514 tgd2;
+    DF515 sv_health;
+
 private:
     EXPLICIT Rtcm1042Message(std::vector<uint8_t> data) NOEXCEPT;
 };
