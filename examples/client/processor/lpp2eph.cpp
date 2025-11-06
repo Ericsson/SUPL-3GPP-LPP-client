3818d6e5a47c3313b048da7e6d30f3a1006b416d
Jonathan Wase <ewasjon@ericsson.com>
2025-11-06 10:06:45 +0100

    fix: correct ephemeris scale factors for all GNSS systems
    Fixes scale factors in lpp2eph processor:
    - APowerHalf: changed from 0.0625 (2⁻⁴) to 1.0/524288.0 (2⁻¹⁹)
    - Crc/Crs: changed from 0.0625 (2⁻⁴) to 0.03125 (2⁻⁵) for GPS/Galileo
    - Crc/Crs: changed from 0.0625 (2⁻⁴) to 0.015625 (2⁻⁶) for BeiDou
    - keplerToe: changed from 16s to 60s multiplier
    
    Also adds proper Galileo I/NAV (stanModelID=0) support and improves logging
    with satellite names and timestamps.

--- examples/client/processor/lpp2eph.cpp
+++ examples/client/processor/lpp2eph.cpp
@@ -1,19 +1,24 @@
 #include "lpp2eph.hpp"
 
 #include <asn.1/bit_string.hpp>
+#include <generator/rtcm/satellite_id.hpp>
 #include <loglet/loglet.hpp>
 #include <lpp/assistance_data.hpp>
+#include <time/bdt.hpp>
 #include <time/gps.hpp>
+#include <time/gst.hpp>
+#include <time/tai.hpp>
 
 #include <A-GNSS-ProvideAssistanceData.h>
 #include <GNSS-GenericAssistData.h>
 #include <GNSS-GenericAssistDataElement.h>
 #include <GNSS-ID.h>
-#include <GNSS-NavigationModel.h>
 #include <GNSS-NavModelSatelliteElement.h>
+#include <GNSS-NavigationModel.h>
 #include <NavModelKeplerianSet.h>
 #include <NavModelNAV-KeplerianSet.h>
 #include <ProvideAssistanceData-r9-IEs.h>
+#include <StandardClockModelElement.h>
 
 LOGLET_MODULE2(p, lpp2eph);
 #define LOGLET_CURRENT_MODULE &LOGLET_MODULE_REF2(p, lpp2eph)
@@ -46,8 +51,8 @@ void Lpp2Eph::inspect(streamline::System& system, DataType const& message, uint6
     }
 }
 
-void Lpp2Eph::process_gps_navigation_model(streamline::System&           system,
-                                            GNSS_NavigationModel const& nav_model) NOEXCEPT {
+void Lpp2Eph::process_gps_navigation_model(streamline::System&         system,
+                                           GNSS_NavigationModel const& nav_model) NOEXCEPT {
     VSCOPE_FUNCTION();
 
     auto current_week = ts::Gps::now().week();
@@ -60,56 +65,63 @@ void Lpp2Eph::process_gps_navigation_model(streamline::System&           system,
         bool is_keplerian     = sat.gnss_OrbitModel.present == GNSS_OrbitModel_PR_keplerianSet;
         if (!is_nav_keplerian && !is_keplerian) continue;
 
+        auto satellite_id = SatelliteId::from_lpp(SatelliteId::Gnss::GPS, sat.svID.satellite_id);
+        if (!satellite_id.is_valid()) continue;
+
+        auto prn = satellite_id.prn();
+        if (!prn.valid) continue;
+
         auto& clock = sat.gnss_ClockModel.choice.nav_ClockModel;
 
-        // GPS IOD: For L1/CA, bit 1 is '0' followed by 10-bit IODC
-        // Extract 8 LSBs as IODE (bits 3-10 of the 11-bit field)
         helper::BitStringReader iod_reader(&sat.iod);
-        uint8_t  iod  = static_cast<uint8_t>(iod_reader.integer<uint16_t>(3, 8));
-        uint16_t iodc = static_cast<uint16_t>(iod_reader.integer<uint16_t>(1, 10));
+        uint16_t lpp_iod = static_cast<uint16_t>(helper::BitString::from(&sat.iod)->as_int64());
+        uint8_t  iode    = static_cast<uint8_t>(iod_reader.integer<uint16_t>(3, 8));
+        uint16_t iodc    = static_cast<uint16_t>(iod_reader.integer<uint16_t>(1, 10));
 
         ephemeris::GpsEphemeris eph{};
-        eph.prn         = sat.svID.satellite_id;
-        eph.iode        = iod;
-        eph.iodc        = iodc;
-        eph.lpp_iod     = iod;
+        eph.prn  = prn.value;
+        eph.iode = iode;
+        eph.iodc = iodc;
+        // [3GPP TS 37.355]: In the case of broadcasted GPS NAV ephemeris, the iod contains the IODC
+        // as described in [4].
+        eph.lpp_iod     = iodc;
         eph.week_number = current_week;
 
         if (is_nav_keplerian) {
-            auto& kep   = sat.gnss_OrbitModel.choice.nav_KeplerianSet;
-            eph.toe     = kep.navToe * 16;
-            eph.a       = kep.navAPowerHalf * kep.navAPowerHalf * 0.0625;
-            eph.delta_n = kep.navDeltaN * 1e-43 * M_PI;
-            eph.m0      = kep.navM0 * 1e-31 * M_PI;
-            eph.e       = kep.navE * 1e-33;
-            eph.omega   = kep.navOmega * 1e-31 * M_PI;
-            eph.cuc     = kep.navCuc * 1e-29;
-            eph.cus     = kep.navCus * 1e-29;
-            eph.crc     = kep.navCrc * 0.0625;
-            eph.crs     = kep.navCrs * 0.0625;
-            eph.cic     = kep.navCic * 1e-29;
-            eph.cis     = kep.navCis * 1e-29;
-            eph.i0      = kep.navI0 * 1e-31 * M_PI;
-            eph.idot    = kep.navIDot * 1e-43 * M_PI;
-            eph.omega0  = kep.navOmegaA0 * 1e-31 * M_PI;
+            auto& kep     = sat.gnss_OrbitModel.choice.nav_KeplerianSet;
+            eph.toe       = kep.navToe * 16;
+            eph.a         = kep.navAPowerHalf * kep.navAPowerHalf * (1.0 / 524288.0);
+            eph.delta_n   = kep.navDeltaN * 1e-43 * M_PI;
+            eph.m0        = kep.navM0 * 1e-31 * M_PI;
+            eph.e         = kep.navE * 1e-33;
+            eph.omega     = kep.navOmega * 1e-31 * M_PI;
+            eph.cuc       = kep.navCuc * 1e-29;
+            eph.cus       = kep.navCus * 1e-29;
+            eph.crc       = kep.navCrc * 0.03125;
+            eph.crs       = kep.navCrs * 0.03125;
+            eph.cic       = kep.navCic * 1e-29;
+            eph.cis       = kep.navCis * 1e-29;
+            eph.i0        = kep.navI0 * 1e-31 * M_PI;
+            eph.idot      = kep.navIDot * 1e-43 * M_PI;
+            eph.omega0    = kep.navOmegaA0 * 1e-31 * M_PI;
             eph.omega_dot = kep.navOmegaADot * 1e-43 * M_PI;
         } else {
-            auto& kep   = sat.gnss_OrbitModel.choice.keplerianSet;
-            eph.toe     = kep.keplerToe * 16;
-            eph.a       = kep.keplerAPowerHalf * kep.keplerAPowerHalf * 0.0625;
-            eph.delta_n = kep.keplerDeltaN * 1e-43 * M_PI;
-            eph.m0      = kep.keplerM0 * 1e-31 * M_PI;
-            eph.e       = kep.keplerE * 1e-33;
-            eph.omega   = kep.keplerW * 1e-31 * M_PI;
-            eph.cuc     = kep.keplerCuc * 1e-29;
-            eph.cus     = kep.keplerCus * 1e-29;
-            eph.crc     = kep.keplerCrc * 0.0625;
-            eph.crs     = kep.keplerCrs * 0.0625;
-            eph.cic     = kep.keplerCic * 1e-29;
-            eph.cis     = kep.keplerCis * 1e-29;
-            eph.i0      = kep.keplerI0 * 1e-31 * M_PI;
-            eph.idot    = kep.keplerIDot * 1e-43 * M_PI;
-            eph.omega0  = kep.keplerOmega0 * 1e-31 * M_PI;
+            auto& kep     = sat.gnss_OrbitModel.choice.keplerianSet;
+            eph.toe       = kep.keplerToe * 60;
+            eph.a         = kep.keplerAPowerHalf * kep.keplerAPowerHalf * (1.0 / 524288.0);
+            eph.delta_n   = kep.keplerDeltaN * 1e-43 * M_PI;
+            eph.m0        = kep.keplerM0 * 1e-31 * M_PI;
+            eph.e         = kep.keplerE * 1e-33;
+            eph.omega     = kep.keplerW * 1e-31 * M_PI;
+            eph.cuc       = kep.keplerCuc * 1e-29;
+            eph.cus       = kep.keplerCus * 1e-29;
+            eph.crc       = kep.keplerCrc * 0.03125;
+            eph.crs       = kep.keplerCrs * 0.03125;
+            eph.cic       = kep.keplerCic * 1e-29;
+            eph.cis       = kep.keplerCis * 1e-29;
+            eph.i0        = kep.keplerI0 * 1e-31 * M_PI;
+            eph.idot      = kep.keplerIDot * 1e-43 * M_PI;
+            eph.omega0    = kep.keplerOmega0 * 1e-31 * M_PI;
             eph.omega_dot = kep.keplerOmegaDot * 1e-43 * M_PI;
         }
 
@@ -119,87 +131,133 @@ void Lpp2Eph::process_gps_navigation_model(streamline::System&           system,
         eph.toc = clock.navToc * 16;
         eph.tgd = clock.navTgd * 1e-31;
 
+        auto gps_time = ts::Gps::from_week_tow(eph.week_number, static_cast<int64_t>(eph.toe), 0.0);
+        INFOF("GPS ephemeris %s: PRN=%u lpp_iod=%u toe=%s now=%s", satellite_id.name(), eph.prn,
+              eph.lpp_iod, ts::Tai(gps_time).rtklib_time_string().c_str(),
+              ts::Tai::now().rtklib_time_string().c_str());
         system.push(std::move(eph));
-        DEBUGF("GPS ephemeris: PRN=%u IOD=%u IODC=%u", eph.prn, eph.iode, eph.iodc);
     }
 }
 
-void Lpp2Eph::process_gal_navigation_model(streamline::System&           system,
-                                            GNSS_NavigationModel const& nav_model) NOEXCEPT {
+void Lpp2Eph::process_gal_navigation_model(streamline::System&         system,
+                                           GNSS_NavigationModel const& nav_model) NOEXCEPT {
     VSCOPE_FUNCTION();
 
     auto current_week = ts::Gst::now().week();
 
     for (int i = 0; i < nav_model.gnss_SatelliteList.list.count; i++) {
         auto& sat = *nav_model.gnss_SatelliteList.list.array[i];
-        if (sat.gnss_ClockModel.present != GNSS_ClockModel_PR_nav_ClockModel) continue;
+
+        // Only support I/NAV (E1/E5a) for now - stanModelID=0
+        if (sat.gnss_ClockModel.present == GNSS_ClockModel_PR_standardClockModelList) {
+            auto& list       = sat.gnss_ClockModel.choice.standardClockModelList;
+            bool  found_inav = false;
+            for (int j = 0; j < list.list.count; j++) {
+                if (list.list.array[j]->stanModelID && *list.list.array[j]->stanModelID == 0) {
+                    found_inav = true;
+                    break;
+                }
+            }
+            if (!found_inav) {
+                VERBOSEF("Galileo satellite without I/NAV (stanModelID=0), skipping");
+                continue;
+            }
+        } else if (sat.gnss_ClockModel.present != GNSS_ClockModel_PR_nav_ClockModel) {
+            continue;
+        }
 
         bool is_nav_keplerian = sat.gnss_OrbitModel.present == GNSS_OrbitModel_PR_nav_KeplerianSet;
         bool is_keplerian     = sat.gnss_OrbitModel.present == GNSS_OrbitModel_PR_keplerianSet;
         if (!is_nav_keplerian && !is_keplerian) continue;
 
-        auto& clock = sat.gnss_ClockModel.choice.nav_ClockModel;
+        auto satellite_id =
+            SatelliteId::from_lpp(SatelliteId::Gnss::GALILEO, sat.svID.satellite_id);
+        if (!satellite_id.is_valid()) continue;
 
-        // Galileo IOD: Bit 1 is '0' followed by 10-bit IODnav
+        auto prn = satellite_id.prn();
+        if (!prn.valid) continue;
+
+        uint16_t lpp_iod = static_cast<uint16_t>(helper::BitString::from(&sat.iod)->as_int64());
         helper::BitStringReader iod_reader(&sat.iod);
-        uint16_t iod = static_cast<uint16_t>(iod_reader.integer<uint16_t>(1, 10));
+        uint16_t                iod = static_cast<uint16_t>(iod_reader.integer<uint16_t>(1, 10));
 
         ephemeris::GalEphemeris eph{};
-        eph.prn         = sat.svID.satellite_id;
+        eph.prn         = prn.value;
         eph.iod_nav     = iod;
-        eph.lpp_iod     = iod;
+        eph.lpp_iod     = lpp_iod;
         eph.week_number = current_week;
 
         if (is_nav_keplerian) {
-            auto& kep   = sat.gnss_OrbitModel.choice.nav_KeplerianSet;
-            eph.toe     = kep.navToe * 60;
-            eph.a       = kep.navAPowerHalf * kep.navAPowerHalf * 0.0625;
-            eph.delta_n = kep.navDeltaN * 1e-43 * M_PI;
-            eph.m0      = kep.navM0 * 1e-31 * M_PI;
-            eph.e       = kep.navE * 1e-33;
-            eph.omega   = kep.navOmega * 1e-31 * M_PI;
-            eph.cuc     = kep.navCuc * 1e-29;
-            eph.cus     = kep.navCus * 1e-29;
-            eph.crc     = kep.navCrc * 0.0625;
-            eph.crs     = kep.navCrs * 0.0625;
-            eph.cic     = kep.navCic * 1e-29;
-            eph.cis     = kep.navCis * 1e-29;
-            eph.i0      = kep.navI0 * 1e-31 * M_PI;
-            eph.idot    = kep.navIDot * 1e-43 * M_PI;
-            eph.omega0  = kep.navOmegaA0 * 1e-31 * M_PI;
+            auto& kep     = sat.gnss_OrbitModel.choice.nav_KeplerianSet;
+            eph.toe       = kep.navToe * 60;
+            eph.a         = kep.navAPowerHalf * kep.navAPowerHalf * (1.0 / 524288.0);
+            eph.delta_n   = kep.navDeltaN * 1e-43 * M_PI;
+            eph.m0        = kep.navM0 * 1e-31 * M_PI;
+            eph.e         = kep.navE * 1e-33;
+            eph.omega     = kep.navOmega * 1e-31 * M_PI;
+            eph.cuc       = kep.navCuc * 1e-29;
+            eph.cus       = kep.navCus * 1e-29;
+            eph.crc       = kep.navCrc * 0.03125;
+            eph.crs       = kep.navCrs * 0.03125;
+            eph.cic       = kep.navCic * 1e-29;
+            eph.cis       = kep.navCis * 1e-29;
+            eph.i0        = kep.navI0 * 1e-31 * M_PI;
+            eph.idot      = kep.navIDot * 1e-43 * M_PI;
+            eph.omega0    = kep.navOmegaA0 * 1e-31 * M_PI;
             eph.omega_dot = kep.navOmegaADot * 1e-43 * M_PI;
         } else {
-            auto& kep   = sat.gnss_OrbitModel.choice.keplerianSet;
-            eph.toe     = kep.keplerToe * 60;
-            eph.a       = kep.keplerAPowerHalf * kep.keplerAPowerHalf * 0.0625;
-            eph.delta_n = kep.keplerDeltaN * 1e-43 * M_PI;
-            eph.m0      = kep.keplerM0 * 1e-31 * M_PI;
-            eph.e       = kep.keplerE * 1e-33;
-            eph.omega   = kep.keplerW * 1e-31 * M_PI;
-            eph.cuc     = kep.keplerCuc * 1e-29;
-            eph.cus     = kep.keplerCus * 1e-29;
-            eph.crc     = kep.keplerCrc * 0.0625;
-            eph.crs     = kep.keplerCrs * 0.0625;
-            eph.cic     = kep.keplerCic * 1e-29;
-            eph.cis     = kep.keplerCis * 1e-29;
-            eph.i0      = kep.keplerI0 * 1e-31 * M_PI;
-            eph.idot    = kep.keplerIDot * 1e-43 * M_PI;
-            eph.omega0  = kep.keplerOmega0 * 1e-31 * M_PI;
+            auto& kep     = sat.gnss_OrbitModel.choice.keplerianSet;
+            eph.toe       = kep.keplerToe * 60;
+            eph.a         = kep.keplerAPowerHalf * kep.keplerAPowerHalf * (1.0 / 524288.0);
+            eph.delta_n   = kep.keplerDeltaN * 1e-43 * M_PI;
+            eph.m0        = kep.keplerM0 * 1e-31 * M_PI;
+            eph.e         = kep.keplerE * 1e-33;
+            eph.omega     = kep.keplerW * 1e-31 * M_PI;
+            eph.cuc       = kep.keplerCuc * 1e-29;
+            eph.cus       = kep.keplerCus * 1e-29;
+            eph.crc       = kep.keplerCrc * 0.03125;
+            eph.crs       = kep.keplerCrs * 0.03125;
+            eph.cic       = kep.keplerCic * 1e-29;
+            eph.cis       = kep.keplerCis * 1e-29;
+            eph.i0        = kep.keplerI0 * 1e-31 * M_PI;
+            eph.idot      = kep.keplerIDot * 1e-43 * M_PI;
+            eph.omega0    = kep.keplerOmega0 * 1e-31 * M_PI;
             eph.omega_dot = kep.keplerOmegaDot * 1e-43 * M_PI;
         }
 
-        eph.af0 = clock.navaf0 * 1e-31;
-        eph.af1 = clock.navaf1 * 1e-43;
-        eph.af2 = clock.navaf2 * 1e-55;
-        eph.toc = clock.navToc * 60;
+        // Extract clock parameters based on model type
+        if (sat.gnss_ClockModel.present == GNSS_ClockModel_PR_standardClockModelList) {
+            auto&                        list = sat.gnss_ClockModel.choice.standardClockModelList;
+            StandardClockModelElement_t* inav_clock = nullptr;
+            for (int j = 0; j < list.list.count; j++) {
+                if (list.list.array[j]->stanModelID && *list.list.array[j]->stanModelID == 0) {
+                    inav_clock = list.list.array[j];
+                    break;
+                }
+            }
+            if (!inav_clock) continue;  // Should not happen due to earlier check
 
+            eph.af0 = inav_clock->stanClockAF0 * 1e-31;
+            eph.af1 = inav_clock->stanClockAF1 * 1e-43;
+            eph.af2 = inav_clock->stanClockAF2 * 1e-55;
+            eph.toc = inav_clock->stanClockToc * 60;
+        } else {
+            auto& clock = sat.gnss_ClockModel.choice.nav_ClockModel;
+            eph.af0     = clock.navaf0 * 1e-31;
+            eph.af1     = clock.navaf1 * 1e-43;
+            eph.af2     = clock.navaf2 * 1e-55;
+            eph.toc     = clock.navToc * 60;
+        }
+
+        auto gal_time = ts::Gst::from_week_tow(eph.week_number, static_cast<int64_t>(eph.toe), 0.0);
+        INFOF("Galileo ephemeris: PRN=%u lpp_iod=%u toe=%s", eph.prn, eph.lpp_iod,
+              ts::Tai(gal_time).rtklib_time_string().c_str());
         system.push(std::move(eph));
-        DEBUGF("Galileo ephemeris: PRN=%u IOD=%u", eph.prn, eph.iod_nav);
     }
 }
 
-void Lpp2Eph::process_bds_navigation_model(streamline::System&           system,
-                                            GNSS_NavigationModel const& nav_model) NOEXCEPT {
+void Lpp2Eph::process_bds_navigation_model(streamline::System&         system,
+                                           GNSS_NavigationModel const& nav_model) NOEXCEPT {
     VSCOPE_FUNCTION();
 
     auto current_week = ts::Bdt::now().week();
@@ -209,29 +267,35 @@ void Lpp2Eph::process_bds_navigation_model(streamline::System&           system,
         if (sat.gnss_ClockModel.present != GNSS_ClockModel_PR_bds_ClockModel_r12) continue;
         if (sat.gnss_OrbitModel.present != GNSS_OrbitModel_PR_bds_KeplerianSet_r12) continue;
 
+        auto satellite_id = SatelliteId::from_lpp(SatelliteId::Gnss::BEIDOU, sat.svID.satellite_id);
+        if (!satellite_id.is_valid()) continue;
+
+        auto prn = satellite_id.prn();
+        if (!prn.valid) continue;
+
         auto& kep   = sat.gnss_OrbitModel.choice.bds_KeplerianSet_r12;
         auto& clock = sat.gnss_ClockModel.choice.bds_ClockModel_r12;
 
-        // BDS B1I/B3I IOD: 11 MSB bits of toe (scale factor 512)
+        uint16_t lpp_iod = static_cast<uint16_t>(helper::BitString::from(&sat.iod)->as_int64());
         helper::BitStringReader iod_reader(&sat.iod);
         uint16_t iod_11bit = static_cast<uint16_t>(iod_reader.integer<uint16_t>(0, 11));
         uint8_t  iod       = static_cast<uint8_t>(iod_11bit >> 6);
 
         ephemeris::BdsEphemeris eph{};
-        eph.prn         = sat.svID.satellite_id;
+        eph.prn         = prn.value;
         eph.iode        = iod;
         eph.iodc        = iod;
-        eph.lpp_iod     = iod;
+        eph.lpp_iod     = lpp_iod;
         eph.toe         = kep.bdsToe_r12 * 8;
-        eph.a           = kep.bdsAPowerHalf_r12 * kep.bdsAPowerHalf_r12 * 0.0625;
+        eph.a           = kep.bdsAPowerHalf_r12 * kep.bdsAPowerHalf_r12 * (1.0 / 524288.0);
         eph.delta_n     = kep.bdsDeltaN_r12 * 1e-43 * M_PI;
         eph.m0          = kep.bdsM0_r12 * 1e-31 * M_PI;
         eph.e           = kep.bdsE_r12 * 1e-33;
         eph.omega       = kep.bdsW_r12 * 1e-31 * M_PI;
         eph.cuc         = kep.bdsCuc_r12 * 1e-31;
         eph.cus         = kep.bdsCus_r12 * 1e-31;
-        eph.crc         = kep.bdsCrc_r12 * 0.0625;
-        eph.crs         = kep.bdsCrs_r12 * 0.0625;
+        eph.crc         = kep.bdsCrc_r12 * 0.015625;
+        eph.crs         = kep.bdsCrs_r12 * 0.015625;
         eph.cic         = kep.bdsCic_r12 * 1e-31;
         eph.cis         = kep.bdsCis_r12 * 1e-31;
         eph.i0          = kep.bdsI0_r12 * 1e-31 * M_PI;
@@ -244,7 +308,9 @@ void Lpp2Eph::process_bds_navigation_model(streamline::System&           system,
         eph.toc         = clock.bdsToc_r12 * 8;
         eph.week_number = current_week;
 
+        auto bds_time = ts::Bdt::from_week_tow(eph.week_number, static_cast<int64_t>(eph.toe), 0.0);
+        INFOF("BeiDou ephemeris: PRN=%u lpp_iod=%u toe=%s", eph.prn, eph.lpp_iod,
+              ts::Tai(bds_time).rtklib_time_string().c_str());
         system.push(std::move(eph));
-        DEBUGF("BeiDou ephemeris: PRN=%u IOD=%u", eph.prn, eph.iode);
     }
 }
