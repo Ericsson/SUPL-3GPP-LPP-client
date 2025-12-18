c78484f6e17ee29cf26a47ed94fced46febd5dcc
Jonathan Wase <ewasjon@ericsson.com>
2025-12-18 15:47:57 +0100

    chore(build): c++11 issues

--- dependency/io/registry.cpp
+++ dependency/io/registry.cpp
@@ -30,7 +30,9 @@ bool StreamRegistry::has(std::string const& id) const NOEXCEPT {
 
 bool StreamRegistry::schedule_all(scheduler::Scheduler& scheduler) NOEXCEPT {
     VSCOPE_FUNCTIONF("%p, count=%zu", &scheduler, mStreams.size());
-    for (auto& [id, stream] : mStreams) {
+    for (auto& is : mStreams) {
+        auto& id = is.first;
+        auto& stream = is.second;
         if (stream->state() == Stream::State::Initial) {
             DEBUGF("scheduling stream: %s", id.c_str());
             if (!stream->schedule(scheduler)) {
@@ -44,7 +46,9 @@ bool StreamRegistry::schedule_all(scheduler::Scheduler& scheduler) NOEXCEPT {
 
 void StreamRegistry::cancel_all() NOEXCEPT {
     VSCOPE_FUNCTION();
-    for (auto& [id, stream] : mStreams) {
+    for (auto& is : mStreams) {
+        auto& id = is.first;
+        auto& stream = is.second;
         DEBUGF("cancelling stream: %s", id.c_str());
         stream->cancel();
     }
